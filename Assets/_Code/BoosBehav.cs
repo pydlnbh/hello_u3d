52122c9f00f9ed47d7c36897ac185be4fc994b00
peiyiding <616256541@qq.com>
2023-04-29 01:15:14 +0800

    modify code

---
+++ Assets/_Code/BoosBehav.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public sealed class BoosBehav : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision == null) 
+        {
+            return;
+        }
+
+        if (!gameObject.TryGetComponent<Spine.Unity.SkeletonAnimation>(out var spSkeAnim)
+            || null == spSkeAnim)
+        {
+            return;
+        }
+
+        spSkeAnim.AnimationState.ClearTrack(1);
+        spSkeAnim.AnimationState.SetAnimation(1, "BeHited", false);
+    }
+}
