930e94e2e486aa6e644965f47b8d8d723656ffd9
peiyiding <peiyiding@weimeng-hosp.com>
2023-05-16 16:35:03 +0800

    modify code

---
+++ Assets/_Code/Anim/MyTween.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Anim
+{
+    /// <summary>
+    /// 缓动动画类
+    /// </summary>
+    public sealed class MyTween
+    {
+        // 缓动动画完成后回调函数
+        private Action _onCompleteAct;
+
+        public void DoScale(Transform t)
+        {
+            MyTweenComponent.TheInstance().StartCoroutine(DoScale_XC(t));
+        }
+
+        /// <summary>
+        /// 协程方式执行缩放
+        /// </summary>
+        /// <param name="t">按钮位置</param>
+        /// <returns></returns>
+        private IEnumerator DoScale_XC(Transform t)
+        {
+            if (null == t)
+            {
+                yield break;
+            }
+
+            for (var i = 0; i < 16; i++)
+            {
+                t.localScale += (Vector3.right + Vector3.up) * (Time.deltaTime * 2f);
+                yield return 1;
+            }
+
+            for (var i = 0; i < 16; i++)
+            {
+                t.localScale -= (Vector3.right + Vector3.up) * (Time.deltaTime * 2f);
+                yield return 1;
+            }
+
+            t.localScale = Vector3.one;
+
+            _onCompleteAct?.Invoke();
+        }
+
+        public void onCompleted(Action val)
+        {
+            _onCompleteAct = val;
+        }
+    }
+}
---
+++ Assets/_Code/FirstSceneBehav.cs
@@ -0,0 +1,34 @@
+﻿// using Anim;
+// using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public sealed class FirstSceneBehav : MonoBehaviour
+{
+    private void Start()
+    {
+        var goButton_StartGame = GameObject.Find("/Canvas/Button_StartGame");
+        goButton_StartGame.GetComponent<Button>()
+            .onClick.AddListener(() =>
+            {
+                // var myTween = new MyTween();
+                // StartCoroutine(myTween.DoScale_XC(goButton_StartGame.transform));
+
+                // myTween.DoScale(goButton_StartGame.transform);
+
+                // C# 扩展机制
+                //goButton_StartGame
+                //    .transform
+                //    .DoScale()
+                //    .onCompleted(() =>
+                //    {
+                //        //跳转到游戏场景
+                //        SceneManager.LoadScene("GameScene");
+                //    });
+
+                // goButton_StartGame.transform.DOScale(1f, 0.5f);
+            });
+    }
+
+}
